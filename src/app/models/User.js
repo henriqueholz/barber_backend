254feb35416e107b2ad3791ab2eb7adff26605fe
Henrique Holz de Castro <henrique.castro@el.com.br>
2019-10-30 08:24:56 -0300

    falta migrate

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,25 +1,33 @@
-email: Sequelize.STRING,
-password: Sequelize.VIRTUAL,
-password_hash: Sequelize.STRING,
-provider: Sequelize.BOOLEAN,
-},
-{
-  sequelize,
-}
-);
+import Sequelize, { Model } from "sequelize";
 
-this.addHook('beforeSave', async user => {
-  if (user.password) {
-    user.password_hash = await bcrypt.hash(user.password, 8);
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
+        password_hash: Sequelize.STRING,
+        provider: Sequelize.BOOLEAN
+      },
+      {
+        sequelize
+      }
+    );
   }
-});
-
-return this;
 }
 
-checkPassword(password) {
-  return bcrypt.compare(password, this.password_hash);
-}
-}
+// this.addHook('beforeSave', async user => {
+//   if (user.password) {
+//     user.password_hash = await bcrypt.hash(user.password, 8);
+//   }
+// });
+
+// return this;
+// }
+
+// checkPassword(password) {
+//   return bcrypt.compare(password, this.password_hash);
+// }
+// }
 
 export default User;
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,24 @@
 import Router from "express";
+import multer from "multer";
+import multerConfig from "./config/multer";
 
 import UserController from "./app/controllers/UserController";
-import SessionController from "./app/controllers/SessionCOntroller";
+import SessionController from "./app/controllers/SessionController";
+
 import authMiddleware from "./app/middlewares/auth";
+
 const routes = new Router();
 const upload = multer(multerConfig);
+
 routes.post("/users", UserController.store);
 routes.post("/sessions", SessionController.store);
+
 routes.use(authMiddleware); //só vale pras rotas daqui pra frente
+
 routes.put("/users", UserController.update);
+
 routes.post("./files", upload.single("file"), (req, res) => {
   return res.json({ ok: true });
 });
+
 export default routes;
